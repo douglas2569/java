2383772ceea6179ba24e3473ce534f389fa699b6
douglas2570 <douglas2570@gmail.com>
2023-07-26 18:06:03 -0300

    Update ConnectionDatabase.java

--- src/database/ConnectionDatabase.java
+++ src/database/ConnectionDatabase.java
@@ -7,8 +7,7 @@ import java.sql.SQLException;
 public class ConnectionDatabase {
 
     public Connection getConnection() {
-        try {
-            //Class.forName("com.mysql.jdbc.Driver");
+        try {            
             return DriverManager.getConnection("jdbc:mysql://localhost:3306/portfolio", "root", "root");
         } catch (SQLException exception) {
             throw new RuntimeException(exception);
